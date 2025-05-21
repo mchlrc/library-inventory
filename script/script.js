8f7c9ed9e3a8af94ca0704ceda38a4cc8305411d
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-05-20 19:24:19 -0700

    Create Book Constructor and Library Array
    Initial implementation of the Book constructor
    and the function addBookToLibrary which creates
    a new book object and adds it to the myLibrary
    array.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+const myLibrary = [];
+
+function Book(title) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.id = crypto.randomUUID();
+  if (title) {
+    this.title = title;
+  } else {
+    this.title = "untitled";
+  }
+}
+
+function addBookToLibrary(title) {
+  let book = new Book(title);
+  let bookCount = myLibrary.push(book);
+  console.log(
+    `You now have ${bookCount} book${
+      bookCount > 1 ? "s" : ""
+    } in your myLibrary. The new book's ID is ${book.id}.`
+  );
+}
+
+addBookToLibrary("The Hobbit");
+addBookToLibrary("War and Peace");
+addBookToLibrary();
+
+console.log({ myLibrary });
